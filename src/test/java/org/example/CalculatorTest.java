0d2bfd5dc14f2b1c3b2fa0e4d52a6e8df4dbf607
Ignas Traskevicius <i.traskevicius.ext@sportradar.com>
2023-09-22 13:32:42 +0200

    test

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+  @Test
+  void shouldPass() {
+    assertThat(true).isTrue();
+  }
+}
